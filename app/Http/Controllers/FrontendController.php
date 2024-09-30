a68f3e0c752bfae28902995477ba7daa32b12983
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-09-30 14:36:20 +0600

    update

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -10,82 +10,102 @@ class FrontendController extends Controller
 {
     public function index()
     {
-        $data = [
-
-        ];
+        $data = [];
 
         return view('frontend.index', $data);
     }
 
-    public function shop() {
+    public function shop()
+    {
 
         return view('frontend.shop');
     }
 
-    public function about() {
+    public function about()
+    {
 
         return view('frontend.about');
     }
 
-    public function contact() {
+    public function contact()
+    {
 
         return view('frontend.contact');
     }
 
-    public function cart() {
+    public function cart()
+    {
 
         return view('frontend.cart');
     }
 
-    public function checkout() {
+    public function checkout()
+    {
 
         return view('frontend.checkout');
     }
 
-    public function wishlist() {
+    public function wishlist()
+    {
 
         return view('frontend.wishlist');
     }
 
-    public function single_product($id) {
+    public function single_product($id)
+    {
 
         return view('frontend.single-product');
     }
 
-    public function test() {
+    public function test()
+    {
+        // Start time
+        // $startTime = microtime(true);
 
-        $oldCustomers = DB::connection('sk_customers')->table('tbl_accounts')->limit(200)->get();
+        // // Set execution time limit to 1 hour
+        // set_time_limit(3600);
 
-        // dd($oldCustomers);
+        // $oldCustomers = DB::connection('sk_customers')->table('tbl_accounts')->get();
+        // // dd($oldCustomers);
 
-        foreach ($oldCustomers as $customer) {
-            DB::connection('mysql')->table('users')->insert([
-                'name' => $customer->acc_name,
-                'email' => $customer->email_address,
-                'email_verified_at' => Carbon::now(),
-                'type' => 'client',
-                'password' => bcrypt(12345678),
-                'active_status' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
+        // // Loop through each customer and insert into the new database
+        // foreach ($oldCustomers as $customer) {
+        //     $customer->email_address = $customer->email_address ?: null;
 
-            DB::connection('mysql')->table('customers')->insert([
-                'customer_id' => $customer->acc_id,
-                'user_id' => DB::connection('mysql')->table('users')->max('id'),
-                'name' => $customer->acc_name,
-                'email' => $customer->email_address,
-                'email_verified_at' => Carbon::now(),
-                'contact' => $customer->contact_no,
-                'billing_name' => $customer->acc_name,
-                'billing_address' => $customer->address,
-                'shipping_name' => $customer->acc_name,
-                'shipping_address' => $customer->address,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
-        }
+        //     DB::connection('mysql')->table('users')->insert([
+        //         'name' => $customer->acc_name,
+        //         'email' => $customer->email_address,
+        //         'email_verified_at' => Carbon::now(),
+        //         'type' => 'client',
+        //         'password' => bcrypt(12345678),
+        //         'active_status' => 1,
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now(),
+        //     ]);
 
-        dd('Customers imported successfully.');
+        //     DB::connection('mysql')->table('customers')->insert([
+        //         'customer_id' => $customer->acc_id,
+        //         'user_id' => DB::connection('mysql')->table('users')->max('id'),
+        //         'name' => $customer->acc_name,
+        //         'email' => $customer->email_address,
+        //         'email_verified_at' => Carbon::now(),
+        //         'contact' => $customer->contact_no,
+        //         'billing_name' => $customer->acc_name,
+        //         'billing_address' => $customer->address,
+        //         'shipping_name' => $customer->acc_name,
+        //         'shipping_address' => $customer->address,
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now(),
+        //     ]);
+        // }
+
+        // // End time
+        // $endTime = microtime(true);
+
+        // // Calculate total execution time
+        // $executionTime = $endTime - $startTime;
+
+        // // Output execution time in seconds
+        // dd('Customers imported successfully. Total execution time: ' . $executionTime . ' seconds');
     }
 }
